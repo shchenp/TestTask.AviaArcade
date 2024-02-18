75de8ea4104123b83309c2570b31db878fa08ccf
Nikita <np.shche@gmail.com>
2024-02-18 18:14:15 +0300

    feat: add joystick to rotate player's plane

---
+++ Assets/Scripts/PlayerRotator.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotator : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private FixedJoystick _joystick;
+
+    private void FixedUpdate()
+    {
+        var direction = (Vector3.right * _joystick.Vertical + Vector3.up * _joystick.Horizontal) * _speed;
+        var newRotation = (transform.rotation.eulerAngles + direction);
+        
+        _rigidbody.MoveRotation(Quaternion.Euler(newRotation));
+    }
+}

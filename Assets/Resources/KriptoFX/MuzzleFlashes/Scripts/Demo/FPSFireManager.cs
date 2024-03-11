df52c075c13d98d8178bb7ccbc41ee2c24052574
Nikita <np.shche@gmail.com>
2024-03-11 10:47:49 +0300

    feat: add URP. convert materials and rendering settings

--- Assets/Resources/KriptoFX/MuzzleFlashes/Scripts/Demo/FPSFireManager.cs
+++ Assets/Resources/KriptoFX/MuzzleFlashes/Scripts/Demo/FPSFireManager.cs
@@ -8,28 +8,26 @@ public class FPSFireManager : MonoBehaviour
     public float BulletDistance = 100;
     public GameObject ImpactEffect;
 
-    [SerializeField] private Transform _plane;
-
-    public void Fire()
-    {
-        var impactEffectIstance = Instantiate(ImpactEffect, transform.position, transform.rotation);
-
-        Destroy(impactEffectIstance, 4);
-        
-        RaycastHit hit;
-        var ray = new Ray(_plane.position, transform.forward);
-        if (Physics.Raycast(ray, out hit, BulletDistance)) 
-        {
-            var effect = GetImpactEffect(hit.transform.gameObject);
-            if (effect==null)
-                return;
-            //var effectIstance = Instantiate(effect, hit.point, new Quaternion(), hit.transform) as GameObject;
-            var effectIstance = Instantiate(effect, hit.transform);
-            effectIstance.transform.position = hit.point;
+	void Update () {
+	    if (Input.GetMouseButtonDown(0)) {
             
-            effectIstance.transform.LookAt(hit.point + hit.normal);
-            Destroy(effectIstance, 20);
-        }
+	        RaycastHit hit;
+            var ray = new Ray(transform.position, transform.forward);
+            if (Physics.Raycast(ray, out hit, BulletDistance)) {
+                var effect = GetImpactEffect(hit.transform.gameObject);
+                if (effect==null)
+                    return;
+                var effectIstance = Instantiate(effect, hit.point, new Quaternion()) as GameObject;
+                effectIstance.transform.LookAt(hit.point + hit.normal);
+                Destroy(effectIstance, 20);
+
+                var impactEffectIstance = Instantiate(ImpactEffect, transform.position, transform.rotation) as GameObject;
+
+                Destroy(impactEffectIstance, 4);
+            }
+           
+	    }
+	    
     }
     
     [System.Serializable]
